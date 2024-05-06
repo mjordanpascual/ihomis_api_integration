81fb024356ec33c39504f7c60a6a0b27f76acb8f
@mjordanpascual <mjordan.pascual@gmail.com>
2024-05-06 16:48:25 +0800

    Add Tailwind in Frontend and Routes/Controller file in Backend

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,93 @@ app.get('/users', (req, res) => {
     })
 })
 
+app.get('/users/adm', (req, res) => {
+    const sql = `   SELECT distinct hperson.patlast,
+                        hperson.patfirst,
+                        hperson.patmiddle,
+                        hperson.patsex,
+                        concat (hperson.patlast, ' , ',hperson.patfirst, '  ' ,ifnull(hperson.patmiddle,'')) AS 'patname',
+                    hadmlog.admdate,
+                    hadmlog.admtime,
+                    concat (hward.wardname, ' - ',hroom.rmname, ' - ',hbed.bdname) AS 'room_assign',
+                        (select concat (hpersonal.lastname, ' , ' ,hpersonal.firstname, '  ' ,ifnull(hpersonal.middlename, '')) from hpersonal, hprovider where (hpersonal.employeeid = hprovider.employeeid) and (hprovider.licno = hadmlog.licno)) AS 'admit_doc',
+                        (select concat (hpersonal.lastname, ' . ' ,hpersonal.firstname, '  ' ,ifnull(hpersonal.middlename, '')) from hpersonal, hprovider where (hpersonal.employeeid = hprovider.employeeid) and (hprovider.licno = hadmlog.licno2)) AS 'attend_doc',
+                        hperson.hpercode,
+                    hadmlog.enccode,
+                    hward.wardname,
+                        (select hphiclog.typemem from hphiclog,hpatcon	where hpatcon.memphicnum = hphiclog.phicnum and  hpatcon.enccode = hadmlog.enccode) as cc_phmem,
+                        hadmlog.admclerk/*,
+                    fordischarge = (select orcode from hdocord where hdocord.enccode = hadmlog.enccode and orcode='DISCH')*/
+                    FROM hadmlog,
+                    hpatroom A,
+                        hbed, 
+                    hroom,   
+                    hward,
+                    hperson
+                    WHERE ( hperson.hpercode = hadmlog.hpercode ) and
+                    ( A.bdintkey = hbed.bdintkey ) and  
+                    ( A.enccode = hadmlog.enccode ) and  
+                    ( hroom.rmintkey = A.rmintkey ) and  
+                    ( hward.wardcode = A.wardcode ) and  
+                    A.hprdate = (select max(hprdate) from hpatroom where enccode = A.enccode) and
+                    ( hadmlog.admstat = 'A' ) ORDER BY hperson.patlast`;
+
+    db.query(sql, (err, data) => {
+        if(err) return res.json(err);
+        // return res.json(data);
+        return res.send(data);
+    })
+})
+
+
+app.get('/users/inpatient', (req, res)=>{
+    const sqlQuery = `SELECT * FROM api_user_acc;`
+    db.query(sqlQuery, (err, data) => {
+        if(err) return res.json(err);
+        return res.send(data);
+    })
+})
+
+
+app.get('/useracc', (req, res)=>{
+    const sqlQuery = `SELECT * FROM api_user_acc;`
+    db.query(sqlQuery, (err, data) => {
+        if(err) return res.json(err);
+        return res.send(data);
+    })
+})
+
+
+app.post('/insert', (req,res)=>{
+    const reqBody = {
+        api_userName: req.body.api_userName,
+        api_userPass: req.body.api_userPass,
+        api_userRole: req.body.api_userRole
+    };
+
+    const sql = `INSERT INTO api_user_acc (api_username, api_userPass, api_userRole) VALUES (?, ?, ?)`;
+    db.query(sql, [reqBody.api_userName, reqBody.api_userPass, reqBody.api_userRole]);
+
+    res.send("Data inserted successfully!");
+
+})
+
+
+// app.post('/insert', (req,res) => {
+//     let newUser = new User ({
+//         api_userName: req.body.api_userName,
+//         api_userPass: req.body.api_userPass,
+//         api_userRole: req.body.api_userRole 
+//     });
+
+//     return newUser.save().then((user,error) => {
+//         if(error) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     })
+// })
 
 
 app.listen(8081, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './Home'
 import UIS from './forms/UIS';
 import Dashboard from './pages/Dashboard';
 import Sidebar from './pages/Sidebar';
+import Adm from './forms/Adm';
 
 function App() {
 
@@ -16,7 +17,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="users/adm" element={<Adm />} />
         <Route path="/uis" element={<UIS />} />
         <Route path="/dashboard" element={ <Dashboard /> } ></Route>
       </Routes>
